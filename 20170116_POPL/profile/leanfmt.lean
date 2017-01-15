2d45b5a62aed1e3cb82dfbc7d2d918c0145877d5
Jared Roesch <roeschinc@gmail.com>
2017-01-15 15:28:16 +0100

    Update format example

--- 20170116_POPL/profile/leanfmt.lean
+++ 20170116_POPL/profile/leanfmt.lean
@@ -1,5 +1,7 @@
 open tactic
 
+-- set_option trace.compiler true
+
 namespace fmt
 
 meta def format.concat : list format -> format
@@ -15,12 +17,15 @@ meta def binder (n : format) (ty : format) : format :=
 meta def let_binding (n : format) (ty : format) (val : format) : format :=
     "x" ++ format.space ++ ":" ++ format.space ++ ":=" ++ val
 
-meta def lev : level → format :=
-    fun l, to_string l
+def intersperse {A : Type} (elem : A) : list A → list A
+| [] := []
+| (x :: []) := [x]
+| (x :: xs) := x :: elem :: intersperse xs
 
-meta def levels : list level → format
-| [] := format.nil
-| (l :: ls) := lev l
+meta def exp_app (fmt_exp : expr -> format) (fn : expr) (args : list expr) : format :=
+    fmt_exp fn ++ format.space ++
+    (format.concat $
+        intersperse format.space $ list.map (fun e, "(" ++ fmt_exp e ++ ")") args)
 
 meta def exp : expr → format
 | (expr.elet n ty val body) :=
@@ -29,17 +34,18 @@ meta def exp : expr → format
 | (expr.app f arg) :=
     let fn := expr.get_app_fn (expr.app f arg),
         args := expr.get_app_args (expr.app f arg)
-    in
-    exp fn ++ format.space ++ (format.concat $ list.map exp args)
-| (expr.const c ls) := to_string c ++ ".{}"
+    in exp_app exp fn args
+| (expr.const c _) := to_string c
 | (expr.local_const n n' bi ty) := to_string n
 | (expr.macro _ _ _) := "macro"
 | (expr.pi n bi sort ty) :=
-    "forall" ++ format.space ++ binder (to_string n) (exp sort) ++ "," ++ exp (expr.instantiate_var ty (mk_local n sort))
+    "forall" ++ format.space ++ binder (to_string n) (exp sort) ++ "," ++
+    format.group (format.line ++ format.space ++ exp (expr.instantiate_var ty (mk_local n sort)))
 | (expr.lam n bi ty body) :=
-    "fun" ++ format.space ++ binder (to_string n) (exp ty) ++ "," ++ exp (expr.instantiate_var body (mk_local n ty))
+    "fun" ++ format.space ++ binder (to_string n) (exp ty) ++ "," ++
+        format.group (format.line ++ exp (expr.instantiate_var body (mk_local n ty)))
 | (expr.mvar n ty) := "?" ++ to_string n
-| (expr.sort l) := "Type" ++ format.space ++ lev l
+| (expr.sort _) := "Type"
 | (expr.var i) := "i"
 
 meta def decl : declaration → format
@@ -52,18 +58,15 @@ meta def decl : declaration → format
 
 end fmt
 
-meta def real_concat : list string → string
-| [] := ""
-| (f :: fs) := f ++ real_concat fs
-
+/- Turn on the profiler. -/
 set_option profiler true
 
 meta def main : tactic unit := do
     env ← get_env,
     opts ← get_options,
-    let fs := environment.fold env [] (fun decl decls, fmt.decl decl :: decls) in
-    -- could eagerly conver here and cause a problem
-    monad.mapm (fun f, tactic.trace (format.to_string f opts)) fs,
+    let fs := list.taken 10 $ environment.fold env [] (fun decl decls, fmt.decl decl :: decls) in
+    monad.mapm (fun f, tactic.trace (format.to_string f opts ++ "\n")) fs,
     return ()
 
-run_command main
+/- We can then just run the main function to format all declarations in the environment. -/
+-- run_command main
