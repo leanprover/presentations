97603462771dd09a5112afcb41a05e5fd9133df8
Sebastian Ullrich <sebasti@nullri.ch>
2017-01-16 10:33:47 +0100

    Add separate 'live' links

--- 20170116_POPL/add_live_links.sh
+++ 20170116_POPL/add_live_links.sh
@@ -1,2 +1,2 @@
-sed 's|href="./\(.*\)lean"|href="https://leanprover.github.io/programming_in_lean/?url=https://github.com/leanprover/presentations/blob/master/20170116_POPL/\1lean\&live"|g' index.html > new_index.html
 mv new_index.html index.html
+perl -pe 's|href="./(.*?)lean">(.*?)</a>|<a href="https://github.com/leanprover/presentations/blob/master/20170116_POPL/\1lean">\2</a> <a href="https://leanprover.github.io/programming_in_lean/?url=https://github.com/leanprover/presentations/blob/master/20170116_POPL/\1lean\&live">[live]</a>|g' index.html > new_index.html
